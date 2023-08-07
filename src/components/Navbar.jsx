8c8cf3621f606cbec1fce627bc820174f319bf01
Samad Musiliu <musiliusamad@gmail.com>
2023-08-07 09:22:21 +0100

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import close from '../assets/cross.png'
 
 const MenuLinks = ({ selected, setSelected }) => {
     return (
-        <ul className='flex flex-row space-x-14 md:hidden'>
+        <ul className='flex flex-row md:flex-col space-x-14 md:hidden md:space-x-0 md:items-center md:justify-center md:min-h-screen '>
             {navLinks.map(navLink => (
                 <a key={navLink.id} href={`${navLink.href}`}>
                     <li
@@ -28,26 +28,14 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className="glass fixed top-0 w-full flex flex-row items-center justify-between py-1 px-14 md:px-4 sm:px-1">
+        <div className="glass w-full flex flex-row items-center justify-between py-1 px-14 md:px-4 sm:px-1 sm:pr-3">
             {/* <div className='w-[60px] h-[60px]'> */}
             <img
                 src={logo}
                 alt='Videographer logo'
                 className='object-contain w-[55px] h-[55px]'
             />
-            {/* </div> */}
 
-            {/* <ul className='flex flex-row space-x-14 md:hidden'>
-                {navLinks.map(navLink => (
-                    <li
-                        key={navLink.id}
-                        className={`${selected == navLink.title && 'text-btnColor2'} font-satoshi text-[0.95rem] cursor-pointer`}
-                        onClick={() => setSelected(navLink.title)}
-                    >
-                        {navLink.title}
-                    </li>
-                ))}
-            </ul> */}
             <MenuLinks
                 selected={selected}
                 setSelected={setSelected}
@@ -57,9 +45,7 @@ const Navbar = () => {
                 Get Started
             </button>
 
-
-
-            <div className='hidden md:flex ml-4'>
+            <div className='hidden md:flex z-[90]'>
                 {isOpen ? (
                     <img
                         height={20}
@@ -82,17 +68,11 @@ const Navbar = () => {
                     )
                 }
                 {isOpen && (
-                    <div className='fixed inset-0 z-10 top-65 flex flex-col nav-h bg-black text-gray-50'>
-                        {/* <div className='flex-1 p-4'>
-                            <MenuItems active={active} setActive={setActive} isMobile />
-                        </div>
-                        <div className='p-4 border-t dark:border-nft-black-1 border-nft-gray-1'>
-                            <ButtonGroup router={router} setActive={setActive} />
-                        </div> */}
+                    <div className='fixed -botom-65 inset-0 z-[900] flex flex-col nav-h bg-black text-gray-50'>
                         <MenuLinks selected={setSelected} setSelected={setSelected} isMobile />
-                        <button type='button' className='bg-btnColor2 mt-5 text-gray-50 sm:hidden px-7 py-[9px] border-none shadow-sm rounded-[4px] font-satoshi text-[13px]'>
+                        {/* <button type='button' className='bg-btnColor2 mt-5 text-gray-50 sm:hidden px-7 py-[9px] border-none shadow-sm rounded-[4px] font-satoshi text-[13px]'>
                             Get Started
-                        </button>
+                        </button> */}
                     </div>
                 )}
             </div>
